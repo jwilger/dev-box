d963e796c4d2753a887b8c47c9f8b9efe3685573
John Wilger <johnwilger@gmail.com>
2015-11-20 08:47:46 -0800

    Specify the directory into which to clone dotfiles

--- provision.sh
+++ provision.sh
@@ -19,5 +19,5 @@ rbenv install 2.2.3
 rbenv global 2.2.3
 rbenv rehash
 sudo chsh -s /usr/bin/zsh vagrant
-git clone git@github.com:jwilger/dotfiles.git
+git clone git@github.com:jwilger/dotfiles.git ~/dotfiles
 cd dotfiles; git submodule init; git submodule update; rake update; cd -
